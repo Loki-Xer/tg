b80aee999ba97b2bbd579829bbb8ee181de052f7
Aromal <136228489+Loki-Xer@users.noreply.github.com>
2024-03-28 14:49:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const { TOKEN } = require("./env");
-const { Message } require("./lib/");
+const { Message } = require("./lib/");
 const TelegramBot = require('node-telegram-bot-api');
 
-
 const client = new TelegramBot(TOKEN, { polling: true });
-
 
 client.on('message', (msg) => {
   let message = new Message(client, msg);
