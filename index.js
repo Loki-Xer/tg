5bd09e9b075f8fd62ab65bf31dd06916ccb966d9
Aromal <136228489+Loki-Xer@users.noreply.github.com>
2024-03-28 15:31:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ const client = new TelegramBot(TOKEN, { polling: true });
 client.on('message', async (msg) => {
   try {
     let message = new Message(client, msg);
-    let s = JSON.parse(msg)
-    await message.reply(`Hey ${s}`);
+    await message.reply(msg);
   } catch (error) {
     console.error('Error handling message:', error);
     process.exit(1);
