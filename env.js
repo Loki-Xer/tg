50e25e457512d39fcb834e2552972227d5f9dde3
Aromal <136228489+Loki-Xer@users.noreply.github.com>
2024-03-28 15:06:25 +0530

    Update env.js

--- env.js
+++ env.js
@@ -1,5 +1,6 @@
 const token = "7058438916:AAEP1vgzibhoohx2Jal8qDLn8XU-iUUrz0M";
-
+const port = "3000";
 module.exports = {
-  TOKEN: process.env.TOKEN || token
+  TOKEN: process.env.TOKEN || token,
+  PORT: process.env.PORT || port
 }
