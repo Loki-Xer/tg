5e441cb6af3b5b2ad3879fa37e61971319687167
Aromal <136228489+Loki-Xer@users.noreply.github.com>
2024-03-28 23:01:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const TelegramBot = require('node-telegram-bot-api');
 const { TOKEN } = require("./env");
 const { Message } = require("./lib/");
+const { command } = require("./command");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -14,14 +15,7 @@ client.on('message', async (msg) => {
     if (!msg) return;
     let message = new Message(client, msg);
     if (message.isBot) return;
-      if (message.text === "ping") {
-          const start = new Date().getTime();
-          await message.reply("```Ping!```");
-          const end = new Date().getTime();
-          return await message.reply("*Pong!*\n ```" + (end - start) + "``` *ms*");
-      } else {
-          await message.reply(`wroking ${message.text}`);
-      }
+      await command(message);
   } catch (error) {
     console.error('Error handling message:', error);
     process.exit(1); 
