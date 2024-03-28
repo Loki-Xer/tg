73155d85a6a77d51abeb06f895004a624c574306
Aromal <136228489+Loki-Xer@users.noreply.github.com>
2024-03-28 15:16:35 +0530

    Update env.js

--- env.js
+++ env.js
@@ -1,7 +1,6 @@
-const toBool = (x) => x === "true";
 const token = "7058438916:AAEP1vgzibhoohx2Jal8qDLn8XU-iUUrz0M";
-const port = "3000";
+
 module.exports = {
   TOKEN: process.env.TOKEN || token,
-  PORT: toBool(process.env.PORT || port)
+  PORT: 3000
 }
