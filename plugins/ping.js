c822939ddb62e15bd4d382eefd14d43375d7d33f
Aromal <136228489+Loki-Xer@users.noreply.github.com>
2024-03-28 22:23:06 +0530

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -1,16 +1,20 @@
-const { addCommand } = require("../lib/");
+const { command } = require("../lib/");
 
-addCommand(
-  {
-    pattern: "ping",
-    fromMe: false,
-    desc: "To check ping",
-    type: "user",
-  },
-  async (message) => {
-    const start = new Date().getTime();
-    await message.reply("```Ping!```");
-    const end = new Date().getTime();
-    return await message.reply("*Pong!*\n ```" + (end - start) + "``` *ms*");
-  }
-);
+module.exports = (client) => {
+    command(
+        {
+            pattern: "ping",
+            fromMe: false,
+            desc: "Check the bot's ping",
+            type: "user",
+        },
+        async (message, match) => {
+            const start = new Date().getTime();
+            await message.sendMessage(message.jid, "```Pinging...```");
+            const end = new Date().getTime();
+            return await message.sendMessage(message.jid,
+                "*Pong!*\n ```" + (end - start) + "``` *ms*"
+            );
+        }
+    );
+};
