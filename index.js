7c859d3a31a6b79a2d100211b6efb9f0e94b5a72
Aromal <136228489+Loki-Xer@users.noreply.github.com>
2024-03-29 03:53:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const TelegramBot = require('node-telegram-bot-api');
-const { TOKEN } = require("./env");
 const { Message, command } = require("./lib/");
+let env = require("./env");
+let prefix = (!env.HANDLERS || env.HANDLERS.trim() == 'null' || env.HANDLERS.trim() == 'false') ? '' : env.HANDLERS.trim();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-const client = new TelegramBot(TOKEN, { polling: true });
-console.log("starting !!");
+
+const client = new TelegramBot(env.TOKEN, { polling: true });
+console.log("Bot started!");
 
 client.on('message', async (msg) => {
   try {
@@ -16,39 +18,35 @@ client.on('message', async (msg) => {
     message.command = message.text.replace(prefix, '').trim().split(/ +/).shift().toLowerCase();
     let match = message.text.trim().split(/ +/).slice(1);
     if (message.isBot) return;
-      await command(message, match);
+    await command(message, match);
     if (message.text) {
-      console.log("tg bot"),
-        console.log("[ MESSAGE ]"),
-        console.log(new Date()),
-        console.log(message.user.firstName),
-        console.log(msg.text)
+      console.log("[TG BOT MESSAGE]");
+      console.log(new Date());
+      console.log(message.user.firstName);
+      console.log(msg.text);
     }
   } catch (error) {
     console.error('Error handling message:', error);
-    process.exit(1); 
   }
 });
 
 client.on('callback_query', async (callbackQuery) => {
   try {
-    if (!msg) return;
-    let message = new Message(client, msg);
+    if (!callbackQuery) return;
+    let message = new Message(client, callbackQuery.message);
     message.text = callbackQuery.data;
     message.command = message.text.replace(prefix, '').trim().split(/ +/).shift().toLowerCase();
     let match = message.text.trim().split(/ +/).slice(1);
     if (message.isBot) return;
-      await command(message, match);
+    await command(message, match);
     if (message.text) {
-      console.log("tg bot"),
-        console.log("[ MESSAGE ]"),
-        console.log(new Date()),
-        console.log(msg.pushName),
-        console.log(msg.text)
+      console.log("[TG BOT CALLBACK QUERY]");
+      console.log(new Date());
+      console.log(message.user.firstName);
+      console.log(callbackQuery.data);
     }
   } catch (error) {
-    console.error('Error handling message:', error);
-    process.exit(1); 
+    console.error('Error handling callback query:', error);
   }
 });
 
