722ac4b74a248039ba17212c000d35a66a627fe3
Aromal <136228489+Loki-Xer@users.noreply.github.com>
2024-03-29 03:46:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,8 +13,10 @@ client.on('message', async (msg) => {
   try {
     if (!msg) return;
     let message = new Message(client, msg);
+    message.command = message.text.replace(prefix, '').trim().split(/ +/).shift().toLowerCase();
+    let match = message.text.trim().split(/ +/).slice(1);
     if (message.isBot) return;
-      await command(message);
+      await command(message, match);
     if (message.text) {
       console.log("tg bot"),
         console.log("[ MESSAGE ]"),
@@ -29,9 +31,26 @@ client.on('message', async (msg) => {
 });
 
 client.on('callback_query', (callbackQuery) => {
-  console.log(callbackQuery);
+  try {
+    if (!msg) return;
+    let message = new Message(client, msg);
+    message.text = callbackQuery.data;
+    message.command = message.text.replace(prefix, '').trim().split(/ +/).shift().toLowerCase();
+    let match = message.text.trim().split(/ +/).slice(1);
+    if (message.isBot) return;
+      await command(message, match);
+    if (message.text) {
+      console.log("tg bot"),
+        console.log("[ MESSAGE ]"),
+        console.log(new Date()),
+        console.log(msg.pushName),
+        console.log(msg.text)
+    }
+  } catch (error) {
+    console.error('Error handling message:', error);
+    process.exit(1); 
+  }
 });
-
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
