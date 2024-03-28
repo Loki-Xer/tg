611316a767552317c5e932a031f0dee857a93b07
Aromal <136228489+Loki-Xer@users.noreply.github.com>
2024-03-28 15:01:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
+const express = require('express');
+const TelegramBot = require('node-telegram-bot-api');
 const { TOKEN } = require("./env");
 const { Message } = require("./lib/");
-const TelegramBot = require('node-telegram-bot-api');
+
+const app = express();
+const port = process.env.PORT || 3000;
 
 const client = new TelegramBot(TOKEN, { polling: true });
 
-client.on('message', (msg) => {
-  let message = new Message(client, msg);
-  message.reply(`hy ${message.user.name}`);
+client.on('message', async (msg) => {
+  try {
+    let message = new Message(client, msg);
+    await message.reply(`Hey ${msg}`);
+  } catch (error) {
+    console.error('Error handling message:', error);
+    process.exit(1);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
