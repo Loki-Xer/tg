37ec3cd115b99e698622878d6a735e004a233671
Aromal <136228489+Loki-Xer@users.noreply.github.com>
2024-04-01 15:37:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ client.on('message', async (msg) => {
     if (!msg) return;
     let message = new Message(client, msg);
     message.command = message.text.replace(prefix, '').trim().split(/ +/).shift().toLowerCase();
-    message.match = msg.text.replace(message.command, '').trim();
+    message.match = message.text.replace(message.command, '').trim();
     if (message.isBot) return;
     await command(message);
     if (message.text) {
@@ -36,7 +36,7 @@ client.on('callback_query', async (callbackQuery) => {
     let message = new Message(client, callbackQuery.message);
     message.action = callbackQuery.data;
     message.command = message.action.replace(prefix, '').trim().split(/ +/).shift().toLowerCase();
-    message.match = msg.action.replace(message.command, '').trim();
+    message.match = message.action.replace(message.command, '').trim();
     if (!message.isBot) return;
     await command(message);
     if (message.action) {
