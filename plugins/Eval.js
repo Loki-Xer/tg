7b3a39fdc56c4d061201fa97e9187e1b581b504a
Aromal <136228489+Loki-Xer@users.noreply.github.com>
2024-04-02 19:00:29 +0530

    Create Eval.js

---
+++ plugins/Eval.js
@@ -0,0 +1,17 @@
+const { Command } = require("../lib/");
+
+Command({
+    on: "all",
+}, async (message, match) => {
+    if (message.text.startsWith(">")) {
+        let m = message;
+        const code = message.text.slice(1).trim();
+        try {
+            let evaled = await eval(code);
+            if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
+            return await message.send(evaled);
+        } catch (error) {
+            return await message.send(`Error executing code: ${error.message}`);
+        }
+    }
+});
