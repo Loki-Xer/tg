74739d5feb72356c904bd477f53bd8e3c709c54f
Aromal <136228489+Loki-Xer@users.noreply.github.com>
2024-04-02 17:37:03 +0530

    Create ping.js

---
+++ plugins/ping.js
@@ -0,0 +1,11 @@
+const { cmd } = require("../lib/");
+
+cmd({
+	pattern: "ping",
+	type: "user",
+}, async (message, match) => {
+	const start = new Date().getTime();
+	const ping = await message.reply("*𝆺𝅥 running 𝆺𝅥*");
+	const end = new Date().getTime();
+	return await message.reply("*☇ ꜱᴩᷨᴇͦᴇͭᴅ ☁ :* " + (end - start) + " *ᴍꜱ* ");
+});
