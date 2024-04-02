9cde312447da6bfb2b70e1d724b102fb2dacd5e5
Aromal <136228489+Loki-Xer@users.noreply.github.com>
2024-04-02 20:46:05 +0530

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -10,3 +10,16 @@ Command({
 	const end = new Date().getTime();
 	return await message.reply("*☇ ꜱᴩᷨᴇͦᴇͭᴅ ☁ :* " + (end - start) + " *ᴍꜱ* ");
 });
+
+
+Command({
+	pattern: "runtime",
+	type: "user",
+	fromAdmin: true
+}, async (message, match) => {
+	const duration = process.uptime();
+    const seconds = Math.floor(duration % 60);
+    const minutes = Math.floor((duration / 60) % 60);
+    const hours = Math.floor((duration / (60 * 60)) % 24);
+    await message.reply(`_Runtime: ${hours.toString().padStart(2, "0")} hours ${minutes.toString().padStart(2, "0")} minutes ${seconds.toString().padStart(2, "0")} seconds_`);
+});              
