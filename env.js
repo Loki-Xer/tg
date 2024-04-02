b65bc5df4dbf0eb04e7e3b244646aeded895a989
Aromal <136228489+Loki-Xer@users.noreply.github.com>
2024-04-02 15:01:23 +0530

    Update env.js

--- env.js
+++ env.js
@@ -1,7 +1,7 @@
 const token = "7040611799:AAF9TBAXwm9Pp1wk9aa7TOd3dVaxgQGTVDc";
 
 module.exports = {
-  ADMIN: process.env.ADMIN || "1260610452, 6332950297, 5535023701",
+  ADMIN: process.env.ADMIN || "1260610452, 6332950297, 5535023701, 5691953694",
   TOKEN: process.env.TOKEN || token,
   HANDLERS: process.env.HANDLERS || "/"
 }
