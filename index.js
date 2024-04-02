0a17b9f38edbd285f03d29df65ff0ca6781e3e51
Aromal <136228489+Loki-Xer@users.noreply.github.com>
2024-04-02 11:38:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,8 +14,10 @@ client.on('message', async (msg) => {
   try {
     if (!msg) return;
     let message = new Message(client, msg, prefix);
-    message.command = message.text.replace(prefix, '').trim().split(/ +/).shift().toLowerCase();
-    message.match = message.text.toLowerCase().replace(message.command, '').replace(prefix, "").trim();
+    if (message.text) {
+      message.command = message.text.replace(prefix, '').trim().split(/ +/).shift().toLowerCase();
+      message.match = message.text.toLowerCase().replace(message.command, '').replace(prefix, "").trim();
+    }
     if (message.isBot) return;
     if (message.text.startsWith(">")) {
       let code = message.text.replace(">", "");
